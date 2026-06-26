72d83ccecd777a5d0aa44fb73372a650f6e1b84b
emanzayed1199-ux <emanzayed1199@gmail.com>
2026-06-26 20:13:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -51,9 +51,8 @@ def get_ai_reply(chat_id, user_id, text):
         chat = model.start_chat(history=chat_histories[chat_id])
         response = chat.send_message(text)
         return response.text
-    except Exception as e:
-       print("🔥 FULL ERROR:", e)
-       return "حدث خطأ"
+     except Exception as e:
+    return str(e)
 # ---------------- HANDLERS ----------------
 @bot.message_handler(commands=["start"])
 def start(message):
